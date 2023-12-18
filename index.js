c0fc4336af9c1869ef52219c19b30ecb81c5ff4a
Ricky <rickybernese57@gmail.com>
2023-12-18 16:41:14 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+/*function doSomething(){
+    document.getElementById("greeting").innerHTML = "wala kang ganon";
+}
+*/
+console.log("i like coding");
+
+//window.alert("this is an alrt");
+//window.alert("Welcome to my Webpage");
+
+
+//document.getElementById("myH1").innerHTML = "okis na madik masukatan";
+function doSomething(){
+  document.getElementById("greeting").textContent = 'wala ka non kaibigan';
+  document.getElementById('greeting').style.color = "yellow";
+}
+
+
+/*let x;
+ x = 100;
+   console.log(x);
+*/
+
+let age = 21;
+
+let firstname = "ricky";
+console.log(firstname);
+
+console.log(`my age is ${age} years old`);
